27973744800dd7187ed35712dc44d7a4de91edd3
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-08-24 20:18:36 +0100

    feat: Add of gradient for neural network

--- Neural network/Network.jl
+++ Neural network/Network.jl
@@ -3,44 +3,79 @@ using Metrics
 using Plots: plot
 using ProgressBars
 mutable struct NeuralNetwork
-    W::Array
-    b::Array
+    W::Array{Matrix}
+    b::Array{Matrix}
     α::Float64
     number_per_layer::Array
     """
     init(x::Neuron, X::AbstractMatrix)
-Initialisation of a neural network with random weights and bias
-# Arguments:
-- `x::Neuron`: the neuron to initialize
-- `X::AbstractMatrix`: the input data of size (m, n)
-- `number_per_layer::Array`: the number of neurons per layer 
-"""
-    function NeuralNetwork(number_of_feature, number_per_layer::Array)
+    Initialisation of a neural network with random weights and bias
+    # Arguments:
+    - `number_of_feature::Int64`: number of features of the input data
+    - `number_per_layer::Array`: the number of neurons per layer 
+    """
+    function NeuralNetwork(number_of_feature::Int64, number_per_layer::Array)
         layer = [number_of_feature, number_per_layer...]
-        W = []
-        b = []
-        for i in 2:size(layer)[1]
-            push!(W, randn((layer[i], layer[i-1])))
-            push!(b, randn((layer[i], 1)))
-        end
-        new(W, b, number_per_layer, 0.01)
+        W = [randn((layer[i], layer[i-1])) for i in 2:size(layer)[1]]
+        b = [randn((layer[i], 1)) for i in 2:size(layer)[1]]
+        @show layer
+        new(W, b, 0.01, layer)
     end
 end
 
 
 
-function forward(x::NeuralNetwork, X::AbstractMatrix)
+function forward_propagation(x::NeuralNetwork, X::Array)::Array
     A = []
-    for i in 1:size(x.number_per_layer)[1]
-
+    for i in 1:size(x.number_per_layer)[1]-1
         answer = i == 1 ? X : A[i-1]
-        @show size(A)
         Z = z(x.W[i], answer, x.b[i])
-        @show size(Z)
         push!(A, a(Z))
     end
-    return A
+    return [X, A...]
 end
-x = NeuralNetwork()
-init(x, [1 4 5 6], [2, 1])
-@show forward(x, [1 4 5 6])
+
+function back_propagation(x::NeuralNetwork, X::Array, Y::Array, A::Array)
+    number_of_layer = length(x.number_per_layer) - 1
+    dZ = Array{Any}(undef, number_of_layer)
+    dW = Array{Any}(undef, number_of_layer)
+    db = Array{Any}(undef, number_of_layer)
+    m = size(Y)[1]
+    for i in number_of_layer:-1:1
+        dZ[i] = i == number_of_layer ? A[i] .- Y : (x.W[i+1] * dZ[i+1]) .* (A[i] .* (1 .- A[i]))
+        dW[i] = i == 1 ? (1/m) * (dZ[i] * X') : (1/m) * (dZ[i] * A[i-1]')
+        db[i] = (1 / m) .* sum(dZ[i], dims=2)
+    end
+    return dW, db
+end
+
+function predict(x::NeuralNetwork, data::Array)
+    final_result = forward_propagation(x, data)[end][1]
+    return final_result > 0.5 
+end
+
+function gradient(network::NeuralNetwork, data::AbstractMatrix, y::AbstractMatrix; iter::Int64=10000)
+    loss = []
+    accuracy = []
+    println("Start of gradient")
+    for i in ProgressBar(1:iter)
+        A = forward_propagation(network, data)
+        dW, db = back_propagation(network, data, y, A)
+        neuron.W, neuron.b = update(dW, db, neuron.W, neuron.b, network.α)
+        y_pred = predict(network, data)
+        push!(accuracy, binary_accuracy(y_pred, y))
+        push!(loss, log_loss(A[end], y))
+        dW, db = ∂LW(X, y, A), ∂Lb(X, y, A)
+        neuron.W, neuron.b = update(dW, db, neuron.W, neuron.b, α)
+    end
+    y_pred = predict(neuron, X)
+    println("Accuracy Train Set: ", binary_accuracy(y_pred, y))
+    return W, b, loss, accuracy
+end
+x = NeuralNetwork(2, [2, 1])
+@show predict(x, [4; 8])
+# forw = forward_propagation(x, [4; 8])
+
+# w, b = back_propagation(x, [4; 8], [1; 0], forw)
+# @show w
+# @show b
--- Neural network/function.jl
+++ Neural network/function.jl
@@ -3,42 +3,27 @@ using LinearAlgebra
 
 
 """Fonction d'aggrégation (Forward propagation)"""
-function z(W::Array, X::AbstractMatrix, b::Array)::Array
-    return [W[i] * X .+ b[i] for i in 1:size(W)[1]]
+function z(W::Matrix, X::Array, b::Array)::Matrix
+    return W * X .+ b
 end
 
 
 
 """Fonction d'activation"""
-function a(Z::Array)
-    return 1 ./ (1 .+ exp.(-Z[i]))
+function a(Z::Array)::Matrix
+    return 1 ./ (1 .+ exp.(-Z))
+end
+
+function update(dW::Array, db::Array, W::Array, b::Array, α::Array)
+    new_W = []
+    new_b = []
+    for i in 1:eachindex(dW)
+        push!(new_W, W[i] - α .* dW[i])
+        push!(new_b, b[i] - α .* db[i])
+    end
+    return new_W, new_b
 end
 
 
 
-"""Fonction de cout"""
-function log_loss(A::AbstractMatrix, y::AbstractMatrix, ϵ::Float64=1e-15)
-    m = size(y)[1]
-    return -(1/m) * sum(y .* log.(A .+ ϵ) + (1 .- y) .* log.(1 .- A .+ ϵ))
-end
-
-
-
-"""Gradient"""
-function ∂LW(X::AbstractMatrix, y::AbstractMatrix, A::AbstractMatrix)
-    m = size(y)[1]
-    return m^-1 * (X' * (A - y))
-end
-
-
-function ∂Lb(X::AbstractMatrix, y::AbstractMatrix, A::AbstractMatrix)
-    m = size(y)[1]
-    return m^-1 * sum(A - y)
-end
-
-function update(dW::AbstractMatrix, db::Float64, W::AbstractMatrix, b::Float64, α::Float64)
-    W = W - α * dW
-    b = b - α * db
-    return W, b
-end
 
